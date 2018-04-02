9d3888429dbc75406039b2931207ea88ca94c3d8
Ken Dreyer <kdreyer@redhat.com>
2018-04-02 10:30:46 -0600

    tests: add "release date" z2 date

--- helga_productpages/tests/test_release_date.py
+++ helga_productpages/tests/test_release_date.py
@@ -9,6 +9,7 @@ values = [
     ('helga: rhel 8 release date', 'rhel-8-0'),
     ('helga: osp 13 release date', 'rhosp-13.0'),
     ('helga: osp 13 date', 'rhosp-13.0'),
+    ('helga: rhcs 3.0 z2 date', 'ceph-3-0'),
 ]
 
 
