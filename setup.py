fb32eae321f27dd051f7d1ffb32e6f8304e3247f
Ken Dreyer <kdreyer@redhat.com>
2019-12-02 09:03:06 -0700

    setup.py: initialize pytest_args as a string
    Initialize self.pytest_args as a string. This fixes the TypeError where
    we concatenated 'str' and 'list' objects in run_tests()

--- setup.py
+++ setup.py
@@ -117,7 +117,7 @@ class PyTest(TestCommand):
 
     def initialize_options(self):
         TestCommand.initialize_options(self)
-        self.pytest_args = []
+        self.pytest_args = ''
 
     def finalize_options(self):
         TestCommand.finalize_options(self)
